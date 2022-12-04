906164d157d5d9fe02d9a67dbd42a4d628367302
sudam <sudamkwagh@gmail.com>
2022-12-04 17:22:53 +0530

    remove cors

--- server.js
+++ server.js
@@ -13,11 +13,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.use((req,res, next)=>{
-    res.setHeader('Access-Control-Allow-Origin',"*");
-    res.setHeader('Access-Control-Allow-Headers',"*");
-    next();
-});
 
 // app.use(cors({
 //     origin: '*'
